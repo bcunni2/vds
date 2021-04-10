b3bd21ad9d54e72e55be4bd00331ce48840ed613
brandoncomputer <brandoncomputer@hotmail.com>
2021-04-10 09:56:03 -0500

    Module load changes.

--- compile/compile/loadmodule.ps1
+++ compile/compile/loadmodule.ps1
@@ -1,19 +1,19 @@
 $erpref = $ErrorActionPreference
 $ErrorActionPreference = 'SilentlyContinue'
 $sp = Split-Path -Parent -Path ([Environment]::GetCommandLineArgs()[0])
-if ((Get-AuthenticodeSignature $sp\vds.dll).SignerCertificate.Thumbprint -eq 'EDB87F69A00BF024D88DDF2E97B345CBF5879D35'){
-	import-module $sp\vds.dll
+if ((Get-AuthenticodeSignature $sp\vds.psm1).SignerCertificate.Status -eq 'Valid'){
+	import-module $sp\vds.psm1
 }
 else {
-	if ((get-filehash $sp\vds.dll).hash -eq 'CDE2A58545278E54A220B2AC00C9D621566CB897CC4ACCCB8685CDB482853A3E') {
+	if ((get-filehash $sp\vds.dll).hash -eq '2D3BD382C2FF0E2D6102B38FA3253253BD04C1DED5FF9A8686F10F86F8DB113C') {
 		iex (Get-Content $sp\vds.dll | out-string)
 	}
 	else {
-		if ((Get-AuthenticodeSignature \windows\system32\vds.dll).SignerCertificate.Thumbprint -eq 'EDB87F69A00BF024D88DDF2E97B345CBF5879D35'){
-			import-module \windows\system32\vds.dll
+		if ((Get-AuthenticodeSignature \windows\system32\vds.psm1).SignerCertificate.Status -eq 'Valid'){
+			import-module \windows\system32\vds.psm1
 		}
 		else {
-			if ((get-filehash \windows\system32\vds.dll).hash -eq 'CDE2A58545278E54A220B2AC00C9D621566CB897CC4ACCCB8685CDB482853A3E') {
+			if ((get-filehash \windows\system32\vds.dll).hash -eq '2D3BD382C2FF0E2D6102B38FA3253253BD04C1DED5FF9A8686F10F86F8DB113C') {
 				iex (Get-Content \windows\system32\vds.dll | out-string)
 			}
 			else {
